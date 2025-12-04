ef11558e763e9017fc78064b490ed8371e0978ea
aflanders7 <audrey.flanders117@gmail.com>
2025-12-04 18:10:03 -0500

    remove slash

--- backend/app/core/app.py
+++ backend/app/core/app.py
@@ -13,7 +13,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://disney-planner-app.vercel.app", "https://www.mousedays.net/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://disney-planner-app.vercel.app", "https://www.mousedays.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
