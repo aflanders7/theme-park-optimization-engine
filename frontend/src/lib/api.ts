f2a546177d39fc02bb3c83e5a20871f9fd9827cf
aflanders7 <audrey.flanders117@gmail.com>
2026-07-28 20:57:45 -0400

    create sample env files

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,8 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
-  //baseURL: 'https://disney-planner-app-production.up.railway.app/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
